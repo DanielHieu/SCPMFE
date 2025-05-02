f41a2aee0e82a727ba64e5708e8998653024955a
DanielHieu <47274268+DanielHieu@users.noreply.github.com>
2025-05-02 15:57:32 +0700

    remove bell and setting in header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -69,11 +69,6 @@ export default function Header({ toggleSidebarAction, isScrolled = false }: Head
 
       {/* Right side - Notifications, settings, user menu */}
       <div className="flex items-center space-x-4">
-        {/* Notifications */}
-        <Button variant="ghost" size="icon" className="text-gray-500 hover:text-gray-700">
-          <Bell className="h-5 w-5" />
-        </Button>
-
         {/* User dropdown */}
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
@@ -94,10 +89,6 @@ export default function Header({ toggleSidebarAction, isScrolled = false }: Head
               </div>
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuItem className="cursor-pointer">
-              <Settings className="w-4 h-4 mr-2" />
-              <span>Settings</span>
-            </DropdownMenuItem>
             <DropdownMenuSeparator />
             <DropdownMenuItem className="cursor-pointer text-red-500 focus:text-red-500" onClick={handleSignOut}>
               <LogOut className="w-4 h-4 mr-2" />
